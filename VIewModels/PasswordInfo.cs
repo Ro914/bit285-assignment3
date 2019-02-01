40c8b24230cd7d40b3663b5baa34b9c0c8f7224e
Brian Bansenauer <brianb@cascadia.edu>
2019-01-31 23:07:17 -0800

    fixed viewmodel

--- VIewModels/PasswordInfo.cs
+++ VIewModels/PasswordInfo.cs
@@ -1,10 +1,21 @@
 ﻿using System;
-namespace bit285assignment2login.VIewModels
+using System.ComponentModel.DataAnnotations;
+
+namespace bit285_assignment2_login.ViewModels
 {
     public class PasswordInfo
     {
-        public PasswordInfo()
-        {
-        }
+        //Used for passing data
+        public long Id { get; set; }
+        public string Password { get; set; }
+
+        [Required(ErrorMessage="*")]
+        public string FavoriteColor { get; set; }
+        [Required(ErrorMessage = "*")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Range(1901,2010,ErrorMessage ="Please enter year between 1900 and 2010")]
+        public int BirthYear { get; set; }
+
     }
 }
