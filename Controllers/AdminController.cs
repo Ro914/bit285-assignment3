defbb5b3f1fd13dc580da231b064cea997cc9f50
Eric Ro <erictro@student.cascadia.edu>
2019-03-12 15:28:37 -0700

    asdsa

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using bit285_assignment3_api.Models;
+using bit285_assignment3_api.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace bit285_assignment3_api.Controllers
+{
+    public class AdminController : Controller
+    {
+        private BitDataContext _dbc;
+
+        public AdminController(BitDataContext dbc)
+        {
+            _dbc = dbc;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ViewModels/UserAdmin.cs
+++ ViewModels/UserAdmin.cs
@@ -7,6 +7,10 @@ namespace bit285_assignment3_api.ViewModels
     public class UserAdmin
     {
         //TODO : Add properties to support the /Views/Admin/Index.cshtml View
+        public long uId { get; set; }
 
+        public string FullName { get; set; }
+
+        public ICollection<User> Users { get; set; }
     }
 }
