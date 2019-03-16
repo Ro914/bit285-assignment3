6ef1c0d48f0511fefe1c2646cd693304b87529ae
Eric Ro <erictro@student.cascadia.edu>
2019-03-16 03:27:40 -0700

    Revert "Revert "finish + 1 bonus""
    This reverts commit 7b0fe4b67d5df6181aaa766cf9550cc57d5ac612.

--- Migrations/20190316063057_InitialCreate.Designer.cs
+++ Migrations/20190316063057_InitialCreate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using bit285_assignment3_api.Models;
 
 namespace bit285_assignment3_api.Migrations
 {
     [DbContext(typeof(BitDataContext))]
-    partial class BitDataContextModelSnapshot : ModelSnapshot
+    [Migration("20190316063057_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
